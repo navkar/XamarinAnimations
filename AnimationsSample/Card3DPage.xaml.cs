a8299411292ed2c8ec83380702d52f857ffea206
Naveen Karamchetti <navkar@outlook.com>
2020-08-13 14:05:30 -0500

    Gradients-demo using gradient magic

---
+++ AnimationsSample/Card3DPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AnimationsSample
+{
+    public partial class Card3DPage : ContentPage
+    {
+        private bool _isAnimating;
+
+        public Card3DPage()
+        {
+            InitializeComponent();
+        }
+
+        async void StartStopAnimation_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (!_isAnimating)
+            {
+                _isAnimating = true;
+
+                await Task.WhenAll(
+                  card.RotateTo(40, 10000),
+                  card.RotateXTo(40, 10000),
+                  card.RotateYTo(40, 10000)
+                );
+            }
+            else
+            {
+                _isAnimating = false;
+                ViewExtensions.CancelAnimations(card);
+            }
+        }
+    }
+}
---
+++ AnimationsSample/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AnimationsSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Card3D_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new Card3DPage());
+        }
+
+        async void Shake_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new ProfileShakePage());
+        }
+
+        async void Scale2X_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new ScaleTo2xPage());
+        }
+
+        async void Bounce_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new MacIconBouncePage());
+        }
+
+        async void Gradient_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new PancakeGradientShiftsPage());
+        }
+    }
+}
